f51da60aedfa6d2ef004cb59fef7fb11f9f59791
Stefan Chambov <stefan.cambov2000@gmail.com>
2025-02-28 16:35:02 +0100

    Small style changes

--- src/components/LineThing.tsx
+++ src/components/LineThing.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function LineThing({ leftSideText, rightSideText }: Props) {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex text-xl items-center justify-between">
       <div>{leftSideText}</div>
       <div className="flex-grow border-b border-black-400 mx-2"></div>
       <div>{rightSideText}</div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <div className="mt-18 flex justify-between text-2xl">
       <a href="/">Sara Logo</a>
-      <div className="flex gap-18">
+      <div className="flex gap-12">
         <a href="#work" className="cursor-pointer" onClick={(e) => scrollToWork(e)}>
           Work
         </a>
