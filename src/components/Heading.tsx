934d2cc2f5f3e4cad99108a1e7bba033b96de626
Stefan Chambov <stefan.cambov2000@gmail.com>
2025-04-30 19:26:15 +0200

    Changing margin top for mobile

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import poloroidImg from "../assets/me/poloroid.png";
 
 export default function Heading() {
   return (
-    <div className="mt-10 lg:mt-4 flex flex-col lg:flex-row justify-between gap-4">
+    <div className="mt-4 lg:mt-4 flex flex-col lg:flex-row justify-between gap-4">
       <img src={poloroidImg} className="w-auto h-90 lg:h-140 mx-auto lg:mx-0" />
       <div className="font-instrument text-left text-2xl lg:text-4xl my-auto tracking-tight leading-tight mx-auto lg:mx-0">
         I’m Sara, a <span className="underline">UX/UI</span> and <span className="underline">Web Designer</span> based
