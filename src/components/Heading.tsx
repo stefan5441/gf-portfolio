67611c22dc93b2351989e9124c352f562a7072ba
stefan5441 <stefan.cambov2000@gmail.com>
2026-03-13 15:28:56 +0100

    Changing description

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,8 +6,8 @@ export default function Heading() {
       <img src={poloroidImg} className="w-auto h-90 lg:h-140 mx-auto lg:mx-0" />
       <div className="font-instrument text-left text-2xl lg:text-4xl my-auto tracking-tight leading-tight mx-auto lg:mx-0">
         I’m Sara, a <span className="underline">UX/UI</span> and <span className="underline">Web Designer</span> based
-        in Skopje, Macedonia. Currently working at XSoft, where I design in <span className="underline">Figma</span> and
-        bring ideas to life using our custom website-building platform.
+        in Skopje, Macedonia. I focus on designing intuitive user experiences and visually strong websites, using{" "}
+        <span className="underline">Figma</span> to transform ideas into functional digital products.
       </div>
     </div>
   );
